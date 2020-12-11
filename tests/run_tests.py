540f73f8fdef19fa0a9e103960b8dfae00d777eb
sam-black <ferlucy222@gmail.com>
2020-12-11 18:53:20 -0500

    added test_scaffold

---
+++ tests/run_tests.py
@@ -0,0 +1,16 @@
+import pytest
+import pandas as pd
+import numpy as np
+
+
+
+"""
+test cases
+
+1. test round to nearest increment
+2. test creating time series state
+    can access from pd.dataframe, np.array, or list
+    if window size does not match, test exception
+3. test overriding of feature vector function with custom class
+
+"""
